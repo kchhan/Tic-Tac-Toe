adf84e7759334dc7f55958319ef45a04b4a3196b
Kevin Chhan <kvnchhan@gmail.com>
2020-05-28 12:30:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic-Tac-Toe
+
+A simple tic-tac-toe game created using HTML, CSS, and vanilla Javascript. I focused on using the module pattern and factory functions with my Javascript with the intention of reducing the number of global varaibles accessible and keeping the code more orgainized.
---
+++ app.js
@@ -0,0 +1,130 @@
+// Initialize game board
+const gameBoard = (() => {
+  const board = () => {
+    let _container = document.querySelector('.grid-container');
+    let _grid = document.createElement('div');
+    _grid.classList.add('grid')
+    for(let i = 1; i <= 9; i++) {
+      let _cell = document.createElement('div');
+      _cell.className = `grid-item cell-${i}`;
+      _cell.value = i;
+      _grid.appendChild(_cell);
+    };
+    _container.appendChild(_grid);
+  };
+
+  return board 
+})();
+gameBoard();
+
+// Game will continue until win is true
+let win = false;
+
+// Factory Function for players
+const playerFactory = (player) => {
+  let hand = [];
+
+  const _play = (e) => {
+    if (e.target.classList.contains('grid-item') && !e.target.classList.contains('active') && !win) {
+      e.target.classList.add('active');
+      if (player === 'one') {
+        e.target.innerHTML = `
+          <i class="fas fa-times fa-6x"></i>
+        `;
+        // Push value to playerOne's hand
+        playerOne.hand.push(e.target.value);
+        _showAlert("Player Two's turn", 'neutral')
+      } else  {
+        e.target.innerHTML = `
+          <i class="far fa-circle fa-5x"></i>
+        `;
+        // Push value to playerTwo's hand
+        playerTwo.hand.push(e.target.value)
+        _showAlert("Player One's turn", 'neutral')
+      };
+      // Checks if play makes a winning combo or a draw
+      _checkForWin();
+      _checkForDraw();
+      // Changes to other player for next turn if game continues
+      player = player === 'one' ? 'two' : 'one';
+    };
+  };
+      
+  // Event Listener for each cell
+  document.querySelector('.grid-container').addEventListener('click', _play);
+
+  // All possible winning combinations
+  const _winningCombos = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ];
+  
+  // Checks current player's hand if there is a combo
+  const _checkForWin = ((current) => {
+    current = player === 'one' ? current = playerOne : current = playerTwo;
+    for (let i = 0; i < _winningCombos.length; i++) {
+      // Return true if hand includes a winning combo
+      win = _winningCombos[i].every(value => current.hand.includes(value));
+      if (win) {
+        _showAlert(`GAME! PLAYER ${player.toUpperCase()} WINS!`, 'win')
+        break;
+      };
+    };
+  });
+
+  // Checks if game is a draw
+  const _checkForDraw = () => {
+    const _grid = document.querySelector('.grid');
+    const children = Array.prototype.slice.call(_grid.childNodes);
+
+    function _containsActive(e) {
+      if (e.classList.contains('active')) {
+        return true;
+      }
+      return false;
+    }
+    
+    // Returns true if every node is active
+    let _draw = children.every(_containsActive);
+    if (_draw) {
+      win = true;
+      _showAlert("It's a Draw!", 'draw')
+    }
+  }
+
+  // Changes alert depending on situation
+  const _showAlert = (message, className) => {
+    const messageBox = document.querySelector('#message-box');
+    messageBox.className = className;
+    messageBox.textContent = message;
+  };
+
+  // Resets Game
+  document.querySelector('.reset').addEventListener('click', function(){
+    win = false;
+    playerOne.hand = [];
+    playerTwo.hand = [];
+    let _grid = document.querySelector('.grid');
+    document.querySelector('.grid-container').removeChild(_grid);
+    player = 'one';
+    gameBoard();
+    _showAlert('Game Reset. Go Player One', 'neutral')
+  });
+
+  return {hand};
+};
+const playerOne = playerFactory('one');
+const playerTwo = playerFactory('two');
+
+
+
+// let _draw = false;
+// for(let i = 0; i < _grid.childNodes.length; i++) {
+//   if ()
+// }
